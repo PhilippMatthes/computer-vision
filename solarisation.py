597c3e241c11bae455aa406112eb1c8316864965
PhilippMatthes <github@philippmatth.es>
2018-10-23 11:19:28 +0200

    Add possibility to print polynomial

--- solarisation.py
+++ solarisation.py
@@ -1,20 +1,22 @@
 import numpy as np
-import cv2 as cv
+import matplotlib.pyplot as plt
 
 from image_processing import show_image, read_image_grayscale
 
 
-def solarize_image(x1, y1, x2, y2, img):
-
+def calculate_polynomial(x1, y1, x2, y2):
     # Calculate polynomial
-    a = np.array([[x1*x1*x1, x1*x1, x1, 1],
-                  [x2*x2*x2, x2*x2, x2, 1],
-                  [3*x1*x1, 2*x1, 1, 0],
-                  [3*x2*x2, 2*x2, 1, 0]])
+    a = np.array([[x1 * x1 * x1, x1 * x1, x1, 1],
+                  [x2 * x2 * x2, x2 * x2, x2, 1],
+                  [3 * x1 * x1, 2 * x1, 1, 0],
+                  [3 * x2 * x2, 2 * x2, 1, 0]])
     b = np.array([y1, y2, 0, 0])
     e = np.linalg.solve(a, b)
+    return e
 
-    print("Computed polynomial: y = f(x) = {}x^3 + {}x^2 + {}x^1 + {}".format(e[0], e[1], e[2], e[3]))
+
+def adjust_image(polynomial, img):
+    assert len(polynomial) == 4
 
     # grab the image dimensions
     h = img.shape[0]
@@ -23,13 +25,24 @@ def solarize_image(x1, y1, x2, y2, img):
     for y in range(0, h):
         for x in range(0, w):
             c = img[y, x]
-            value = e[0] * (c*c*c) + e[1] * (c*c) + e[2] * c + e[3]
+            value = polynomial[0] * pow(c, 3) + polynomial[1] * pow(c, 2) + polynomial[2] * c + polynomial[3]
             img[y, x] = value
 
     return img
 
 
+def visualize_polynomial(polynomial, low=0, high=255):
+    values = [polynomial[0] * pow(c, 3) + polynomial[1] * pow(c, 2) + polynomial[2] * c + polynomial[3]
+              for c in range(low, high)]
+    plt.plot(values)
+    plt.show()
+
+
 if __name__ == "__main__":
     img = read_image_grayscale("images/apb.jpg")
-    solarized_img = solarize_image(60, 60, 180, 180, img)
+    polynomial = calculate_polynomial(60, 60, 180, 180)
+    solarized_img = adjust_image(polynomial, img)
+    visualize_polynomial(polynomial)
     show_image(solarized_img)
+
+
